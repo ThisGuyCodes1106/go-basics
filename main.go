5c6eaa067c5160992087ecb4c65f1c3df34d5495
Stefan Madzarac <stefan.madzarac@bjss.com>
2023-08-08 18:05:45 +0100

    up to loops

--- main.go
+++ main.go
@@ -2,10 +2,55 @@ package main
 
 import "fmt"
 
-func sayHi() string {
-	return "Hello World"
+type User struct {
+	FirstName string
+	LastName  string
+	Age       int
+	IsHim     bool
+}
+
+// (info *User) - ties this function to the User struct, giving it access to info from that struct
+func (info *User) greeting() string {
+	return "Hello Mr. " + info.LastName
 }
 
 func main() {
-	fmt.Println(sayHi())
+	user := User{
+		FirstName: "Stefan",
+		LastName:  "Madzarac",
+		Age:       30,
+		IsHim:     false,
+	}
+
+	// map[KeyType]ValueType
+	animals := make(map[string]string)
+
+	animals["Lion"] = "Big Cat"
+
+	profiles := make(map[string]User)
+
+	profiles["me"] = user
+
+	// slice_name := []datatype{values}
+	cars := []string{"Ferrari", "Lamborgini"}
+	cars = append(cars, "Bugatti")
+
+	fmt.Println(user.greeting())
+	fmt.Println(animals["Lion"])
+	fmt.Println(profiles["me"].FirstName)
+	fmt.Println(cars[1:3])
+
+	// Decision Structures
+	if user.IsHim {
+		fmt.Print("Is that guy")
+	}
+
+	// Switch statements
+	switch user.IsHim {
+	case false:
+		fmt.Println("Is not that guy")
+
+	case true:
+		fmt.Println("Is that guy")
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,1 @@
 package main
-
-import "testing"
-
-func testHello(t *testing.T) {
-	got := sayHi()
-	want := "Hello World"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
